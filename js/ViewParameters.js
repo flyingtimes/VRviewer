bf2c075c4e891d2357cabe3c3f10fee2524bd6a4
flyingtimes <5200374@qq.com>
2016-12-28 01:13:00 +0800

    Delete ViewParameters.js

--- js/ViewParameters.js
+++
@@ -1,9 +0,0 @@
-function ViewParameters(){
-    var VRCamera={};
-    var VRScene={};
-    var VROptions={};
-    var VRAmbientLight={};
-    var VRDirectionalLights={};
-}
-
-export { ViewParameters };
